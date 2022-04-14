f76c4434f5723a0072e006f42f73eafacf97d83b
armandcomas <acomasma@gmail.com>
2022-04-14 14:45:38 -0400

    - Encoder with embeddings selected by softmaxed weights

--- aa-concept-dynamics-ebm/encoder_models.py
+++ aa-concept-dynamics-ebm/encoder_models.py
@@ -8,7 +8,7 @@ from downsample import Downsample
 import warnings
 import math
 from torch.nn.utils import spectral_norm as sn
-from third_party.utils import my_softmax, gumbel_softmax
+from third_party.utils import my_softmax, gumbel_softmax, linear_annealing
 from torch.autograd import Variable
 
 warnings.filterwarnings("ignore")
@@ -175,6 +175,7 @@ class CNNLatentEncoder(nn.Module):
 		self.init_weights()
 		self.y_zeros = None
 		self.layernorm = nn.LayerNorm(n_out)
+		self.count = 0
 
 	def init_weights(self):
 		for m in self.modules():
@@ -234,7 +235,9 @@ class CNNLatentEncoder(nn.Module):
 		out = self.fc_out(x)
 
 		### Note: Test feature ###
-		out = my_softmax(out/.1, axis=-1)
+		tmp = linear_annealing(None, self.count, start_step=0, end_step=60000, start_value=1, end_value=0.05)
+		out = my_softmax(out/tmp, axis=-1)
+		self.count += 1
 		# hard = True
 		# if hard:
 		# 	shape = out.size()
@@ -249,5 +252,115 @@ class CNNLatentEncoder(nn.Module):
 		# 	out = Variable(y_hard - out.data) + out
 
 		# out = self.layernorm(out)
-		# out = F.sigmoid(out/.1)
+		# out = F.sigmoid(out)
 		return out
+
+class CNNEmbeddingLatentEncoder(nn.Module):
+	def __init__(self, n_in, n_hid, n_out, do_prob=0., factor=True):
+		super(CNNEmbeddingLatentEncoder, self).__init__()
+		self.dropout_prob = do_prob
+
+		self.factor = factor
+
+		num_class = 4
+		self.cnn = CNN(n_in * 2, n_hid // 2, n_hid, do_prob)
+		self.mlp1 = MLP(n_hid, n_hid, n_hid, do_prob)
+		self.mlp2 = MLP(n_hid, n_hid, n_hid, do_prob)
+		self.mlp3 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
+		self.fc_out = nn.Linear(n_hid, 2 * num_class)
+
+		self.embedding = nn.Embedding(num_class, n_out, padding_idx=0) # Hardcoded
+
+		if self.factor:
+			print("Using factor graph CNN encoder.")
+		else:
+			print("Using CNN encoder.")
+
+		self.init_weights()
+		self.y_zeros = None
+		self.layernorm = nn.LayerNorm(n_out)
+		self.count = 0
+
+	def init_weights(self):
+		for m in self.modules():
+			if isinstance(m, nn.Linear):
+				nn.init.xavier_normal(m.weight.data)
+				m.bias.data.fill_(0.1)
+
+	def node2edge_temporal(self, inputs, rel_rec, rel_send):
+		# NOTE: Assumes that we have the same graph across all samples.
+
+		x = inputs.reshape(inputs.size(0), inputs.size(1), -1)
+
+		receivers = torch.matmul(rel_rec, x)
+		receivers = receivers.reshape(inputs.size(0) * receivers.size(1),
+									  inputs.size(2), inputs.size(3))
+		receivers = receivers.transpose(2, 1)
+
+		senders = torch.matmul(rel_send, x)
+		senders = senders.reshape(inputs.size(0) * senders.size(1),
+								  inputs.size(2),
+								  inputs.size(3))
+		senders = senders.transpose(2, 1)
+
+		# receivers and senders have shape:
+		# [num_sims * num_edges, num_dims, num_timesteps]
+		edges = torch.cat([senders, receivers], dim=1)
+		return edges
+
+	def edge2node(self, x, rel_rec, rel_send):
+		# NOTE: Assumes that we have the same graph across all samples.
+		incoming = torch.matmul(rel_rec.transpose(2, 1), x)
+		return incoming / incoming.size(1)
+
+	def node2edge(self, x, rel_rec, rel_send):
+		# NOTE: Assumes that we have the same graph across all samples.
+		receivers = torch.matmul(rel_rec, x)
+		senders = torch.matmul(rel_send, x)
+		edges = torch.cat([senders, receivers], dim=2)
+		return edges
+
+	def forward(self, inputs, rel_rec, rel_send):
+
+		# Input has shape: [num_sims, num_atoms, num_timesteps, num_dims]
+		edges = self.node2edge_temporal(inputs, rel_rec, rel_send)
+		x = self.cnn(edges)
+		x = x.reshape(inputs.size(0), (inputs.size(1) - 1) * inputs.size(1), -1)
+		x = self.mlp1(x)
+		x_skip = x
+
+		if self.factor:
+			x = self.edge2node(x, rel_rec, rel_send)
+			x = self.mlp2(x)
+
+			x = self.node2edge(x, rel_rec, rel_send)
+			x = torch.cat((x, x_skip), dim=2)  # Skip connection
+			x = self.mlp3(x)
+		out = self.fc_out(x)
+
+		### Note: Test feature ###
+		outs = out.chunk(2, dim=-1)
+		tmp = linear_annealing(None, self.count, start_step=5000, end_step=60000, start_value=10, end_value=0.08)
+		weights = torch.stack([F.softmax(out/tmp, dim=-1) for out in outs], dim=-2)
+		if self.count % 400 == 0:
+			print('Selector: \n{}'.format(weights[0,:,0].detach().cpu().numpy()))
+		out = weights @ self.embedding.weight.clone()[None, None, ...]
+		self.count += 1
+
+		### Note: First tests don't mix
+		out = out[:,:,0]
+
+		# hard = True
+		# if hard:
+		# 	shape = out.size()
+		# 	_, k = out.data.max(-1)
+		# 	# this bit is based on
+		# 	# https://discuss.pytorch.org/t/stop-gradients-for-st-gumbel-softmax/530/5
+		# 	if self.y_zeros is None:
+		# 		self.y_zeros = torch.zeros(*shape)
+		# 		if out.is_cuda:
+		# 			self.y_zeros = self.y_zeros.to(out.device)
+		# 	y_hard = self.y_zeros.zero_().scatter_(-1, k.view(shape[:-1] + (1,)), 1.0)
+		# 	out = Variable(y_hard - out.data) + out
+
+		return (out, weights)
