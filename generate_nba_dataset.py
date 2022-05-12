bdf7344e770f7ef8cc8d38f7f19a9b240cd1b8ab
armandcomas <acomasma@gmail.com>
2022-05-12 12:29:47 -0400

    NBA data generation. CSV to NPY and train/val/test generation

---
+++ generate_nba_dataset.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pandas as pd
+import os
+import time
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--simulation', type=str, default='',
+					help='What simulation to generate.')
+parser.add_argument('--num-train', type=int, default=50000,
+					help='Number of training simulations to generate.')
+parser.add_argument('--num-valid', type=int, default=1000,
+					help='Number of validation simulations to generate.')
+parser.add_argument('--num-test', type=int, default=1000,
+					help='Number of test simulations to generate.')
+parser.add_argument('--length', type=int, default=40,
+					help='Length of trajectory after subsampling.')
+parser.add_argument('--length-test', type=int, default=5000,
+					help='Length of test set trajectory.')
+parser.add_argument('--sample-freq', type=int, default=10,
+					help='How often to sample the trajectory.')
+# parser.add_argument('--n-balls', type=int, default=3,
+# 					help='Number of balls in the simulation.')
+parser.add_argument('--seed', type=int, default=42,
+					help='Random seed.')
+
+args = parser.parse_args()
+
+suffix = '_NBA_11'
+np.random.seed(args.seed)
+
+print(suffix)
+
+datadir = '/data/Armand/nba-data/npy/'
+def generate_dataset(num_sims, length, sample_freq):
+	loc_all = list()
+	vel_all = list()
+	edges_all = list()
+
+
+	num_sims_train, num_sims_val, num_sims_test = num_sims
+
+	splits = []
+	count = 0
+	num_sims_count = 0
+	for file in os.listdir(datadir):
+		array = np.load(datadir + file)
+		if array.shape[1] < (length * sample_freq):
+			continue
+		else: res = array.shape[1] % (length * sample_freq)
+		num_chunks = array.shape[1] // (length * sample_freq)
+		for i in range(num_chunks):
+			subarray = array[:, (length * sample_freq)*(i):(length * sample_freq)*(i+1):sample_freq]
+			loc_all.append(subarray)
+			count += 1
+			if num_sims_count > 2: break
+			if count == num_sims[num_sims_count]:
+				loc_array = np.stack(loc_all)
+				splits.append(loc_array)
+				num_sims_count += 1
+				count = 0
+				break
+		if num_sims_count > 2: break
+	return splits
+
+print("Generating {},{},{} simulations".format(args.num_train, args.num_valid, args.num_test))
+loc_train, loc_valid, loc_test = generate_dataset( [args.num_train, args.num_valid, args.num_test],
+													 args.length,
+													 args.sample_freq)
+
+suffix += '_len{}_sf{}'.format(args.length, args.sample_freq)
+# '_{}sims.npy'.format(args.num_train)
+np.save(datadir + 'loc_train' + suffix + '.npy', loc_train)
+np.save(datadir + 'loc_valid' + suffix + '.npy', loc_valid)
+np.save(datadir + 'loc_test' + suffix + '.npy', loc_test)
+
+
+# import matplotlib.pyplot as plt
+# def visualize_trajectories(list, list_idx=0, len=50, sr=5):
+# 	fig = plt.figure()
+# 	axes = plt.gca()
+# 	lw = 1.5
+# 	sz_pt = 30
+# 	maps = ['bone', 'magma', 'spring', 'autumn', 'gist_gray', 'afmhot', 'cool', 'Wistia', 'YlGnBu'] #https://matplotlib.org/2.0.2/examples/color/colormaps_reference.html
+# 	cmap = maps[4]
+# 	alpha = 0.6
+# 	# if lims is not None:
+# 	# 	axes.set_xlim([lims[0], lims[1]])
+# 	# 	axes.set_ylim([lims[0], lims[1]])
+# 	loc = list[list_idx]
+# 	loc = np.transpose(loc, (1,2,0))[:, :2] # L, F, No
+# 	colors = ['b', 'r', 'c', 'y', 'k', 'm', 'g']
+# 	modes = ['-']*loc.shape[-1]
+#
+#
+# 	for i in range(loc.shape[-1]):
+# 		# plt.scatter(loc[:, 0, i], loc[:, 1, i], s=sz_pt, c=np.arange(loc.shape[1]), cmap=cmap, alpha=alpha)
+# 		# plt.plot(loc[-len*sr::sr, 0, i], loc[-len*sr::sr, 1, i], modes[i], linewidth=lw)
+# 		plt.plot(loc[:len*sr:sr, 0, i], loc[:len*sr:sr, 1, i], modes[i], linewidth=lw)
+# 		plt.plot(loc[0, 0, i], loc[0, 1, i], 'o')
+# 	plt.show()
+#
+# if visualize:
+# 	visualize_trajectories(final_numpy_list, list_idx=-2, len=20)
+
--- process_nba_csv.py
+++ process_nba_csv.py
@@ -3,8 +3,10 @@ import pandas as pd
 import os
 csv_dir = '/data/Armand/nba-movement-data/data/csv/'
 file_id = '0021500172.csv'
-visualize = False
-save = True
+
+visualize = True
+save = False
+by_event = True
 for file_id in os.listdir(csv_dir):
 	data = pd.read_csv(csv_dir+file_id, usecols=['game_id', 'team_id', 'player_id', 'x_loc', 'y_loc', 'quarter', 'game_clock', 'event_id'])
 	data_by_game = data.groupby('game_id')
@@ -30,18 +32,21 @@ for file_id in os.listdir(csv_dir):
 		data_by_event = df_game_i.groupby('event_id')
 		for event_no in data_by_event.groups.keys():
 			df_i = data_by_event.get_group(event_no)
-			if len(all_seconds) > 0:
+			try: assert len(df_i) % 11 == 0
+			except: print('Not divisible by 11, pass'); continue
+			try: assert all(df_i.iloc[::11]['team_id'] == -1)
+			except: print('Divisible by 11, but first player is not -1'); continue
+			if by_event:
+				all_seconds.append(df_i)
+
+
+			elif len(all_seconds) > 0:
 				# print(df_i.iloc[1]['team_id'],all_seconds[-1].iloc[1]['team_id'],
 				# 	  df_i.iloc[-1]['team_id'],all_seconds[-1].iloc[-1]['team_id'], df_i.iloc[-1]['game_clock'])
 				if (df_i.iloc[1]['team_id']==all_seconds[-1].iloc[1]['team_id'] and
 					df_i.iloc[-1]['team_id']==all_seconds[-1].iloc[-1]['team_id'] and
 					df_i.iloc[-1]['quarter']==all_seconds[-1].iloc[-1]['quarter']):
 
-
-					try: assert len(df_i) % 11 == 0
-					except: print('Not divisible by 11, pass'); continue
-					try: assert df_i.iloc[0]['team_id'] == -1
-					except: print('Divisible by 11, but first player is not -1'); continue
 					groups = compare_players_sequentially(df_i)
 					if (df_i.iloc[:11]['player_id'].to_numpy() == all_seconds[-1].iloc[-11:]['player_id'].to_numpy() ).sum()==11:
 						# print(all_seconds[-1], groups[0])
@@ -66,7 +71,7 @@ for file_id in os.listdir(csv_dir):
 		# 	# print(team_group.get_group(team_id))
 		# 	exit()
 		for player_id in players_group.groups.keys():
-			# print(player_id, players_group.get_group(player_id)[['x_loc', 'y_loc']].shape)
+			print(player_id, players_group.get_group(player_id)[['x_loc', 'y_loc','game_clock']])
 			try:
 				array = players_group.get_group(player_id)[['x_loc', 'y_loc']].to_numpy()
 				players.append(array)
@@ -75,14 +80,15 @@ for file_id in os.listdir(csv_dir):
 		zeros = np.zeros(out[:, :, 0:1].shape)
 		zeros[:1] = -1
 		zeros[1:6] = 0
-		zeros[6:11] = 0
+		zeros[6:11] = 1
 		out = np.concatenate([out, zeros], axis=-1)
 		final_numpy_list.append(out)
 
 	[print(elem.shape) for elem in final_numpy_list]
-	final_numpy_list = [elem for elem in final_numpy_list if elem.shape[1]>1]
+	final_numpy_list = [np.flip(elem,1) for elem in final_numpy_list if elem.shape[1]>1]
 
-	if save and not visualize:
+	if visualize: break
+	if save:
 		suffix = 'all_data'
 		for save_id, data in enumerate(final_numpy_list):
 			np.save(csv_dir + file_id[:-4] + '_' + suffix + '{}.npy'.format(save_id), data)
@@ -136,27 +142,3 @@ if visualize:
 		# 	lengths.append(count)
 		# 	count = 0
 		# acc_id += 1
-
-
-
-	# time_stamps = df_game_i['game_clock'].unique()
-	# df_game =df_game_i.copy()
-	#
-	# for i in range(0,df_game_i.shape[0],11):
-	# 	# print(df_game_i['team_id'][i])
-	# 	if df_game_i['team_id'][i] != -1:
-	# 		print(i)
-	# 		break
-
-
-		# rows_t = df_game_i.loc[df_game_i['game_clock']==t]
-		# df_game[loc, 'player_list'] = set(df_game_i.loc[df_game_i['game_clock']==t,'player_id'])
-
-print('high')
-	# for key in data_by_player.groups.keys():
-	#     player_data = data_by_player.get_group(key).sort_values(by='game_clock', ascending=False)
-	#     data_list.append(player_data.to_numpy())
-	# data_np = np.cat(data_list, dim = 0)
-
-# print(data)
-# print(data_by_player)
